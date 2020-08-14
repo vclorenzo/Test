7e9b807a41abd3d7526f784f2c43f149d786a1db
csvelasco <69575616+csvelasco@users.noreply.github.com>
2020-08-14 17:34:35 +0800

    Try (#3)
    * try
    
    * 2nd try
    
    Co-authored-by: kibin <kibin@192.168.0.25>

--- Testttttss/src/samplepack/samplecls.java
+++ Testttttss/src/samplepack/samplecls.java
@@ -6,10 +6,12 @@ public class samplecls {
 	public static void main(String[] args) {
 		
 		String xd = "xaxaxa";
-		
+		String XDXD = "new";
 		System.out.println(xd);
 		System.out.println(xd);
 		System.out.println(xd);
+		System.out.println(XDXD);
+		System.out.println(XDXD);
 	}
 
 }
