f724afba4a39e8dda7bd579c09643834c2099a76
vclorenzo <vc.lorenzo16@gmail.com>
2020-08-14 16:50:37 +0800

    third commit

---
+++ Testttttss/src/samplepack/samplecls.java
@@ -0,0 +1,15 @@
+package samplepack;
+
+public class samplecls {
+	
+	
+	public static void main(String[] args) {
+		
+		String xd = "xaxaxa";
+		
+		System.out.println(xd);
+		System.out.println(xd);
+		System.out.println(xd);
+	}
+
+}
